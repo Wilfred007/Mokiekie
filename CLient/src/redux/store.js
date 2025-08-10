a06afd455c0af4ccd3e91b3bec4176a3e0037206
Wilfred007 <adzerwilfred007@gmail.com>
2025-08-10 22:01:25 +0100

    Added redux persist

--- CLient/src/redux/store.js
+++ CLient/src/redux/store.js
@@ -1,9 +1,90 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+// import { combineReducers, configureStore } from '@reduxjs/toolkit'
+// import userReducer from './user/userSlice'
+// import { persistReducer, persistStore } from 'redux-persist'
+// import { version } from 'mongoose'
+// import storage from 'redux-persist/lib/storage';
+
+
+
+// const rootReducer = combineReducers({
+//     user: userReducer
+// })
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+// const persistConfig = {
+//     key: 'root',
+//     storage,
+//     version: 1,
+// }
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+//     serializableCheck: false,
+//   })
+// })
+
+
+// export const persistor = persistStore(store);
+
+
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import userReducer from './user/userSlice';
+// import { persistReducer, persistStore } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+
+// // ✅ Define persistConfig first
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   version: 1,
+// };
+
+// // Combine reducers
+// const rootReducer = combineReducers({
+//   user: userReducer,
+// });
+
+// // Apply persist reducer after persistConfig exists
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false,
+//     }),
+// });
+
+// export const persistor = persistStore(store);
+
+
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from './user/userSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck: false,
-  })
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
