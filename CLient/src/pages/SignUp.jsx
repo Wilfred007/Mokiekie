19a02b2a51fbd2431f63bcbc462c3c7bc2b46008
Wilfred007 <adzerwilfred007@gmail.com>
2025-08-10 11:36:57 +0100

    implemented sign up ui and functionality

--- CLient/src/pages/SignUp.jsx
+++ CLient/src/pages/SignUp.jsx
@@ -1,20 +1,70 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+
+
+
+
+  const handleChange=(e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(null)
+      const res = await fetch('/Api/auth/signup', 
+        {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const data = await res.json();
+      console.log(data)
+      if(!res.ok){
+        setError(data.message || 'Something went wrong');
+        setLoading(false);
+        return;
+      }
+      setLoading(false)
+      setError(null)
+      navigate('/sign-in')
+      
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+    }
+   
+
+  };
+  // console.log(formData)
   return (
     <div>
       <h1 className='text-3xl text-center font-bold text-slate-600 my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4 p-5 max-w-lg mx-auto'>
-        <input type='text' placeholder='Username' className='border-none bg-white p-3 rounded-lg focus:outline-none' id='username' />
-        <input type='email' placeholder='Email' className='border-none bg-white p-3 rounded-lg focus:outline-none' id='email' />
-        <input type='password' placeholder='Password' className='border-none bg-white focus:outline-none p-3 rounded-lg' id='password' />
-        <button  className='bg-blue-600 text-white p-4 rounded-xl cursor-pointer hover:bg-blue-800 disabled:opacity-85'>Sign Up</button>
+      <form className='flex flex-col gap-4 p-5 max-w-lg mx-auto' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Username' className='border-none bg-white p-3 rounded-lg focus:outline-none' id='username'onChange={handleChange} />
+        <input type='email' placeholder='Email' className='border-none bg-white p-3 rounded-lg focus:outline-none' id='email'onChange={handleChange} />
+        <input type='password' placeholder='Password' className='border-none bg-white focus:outline-none p-3 rounded-lg' id='password'onChange={handleChange} />
+        <button disabled={loading}  className='bg-blue-600 text-white p-4 rounded-xl cursor-pointer hover:bg-blue-800 disabled:opacity-85'>{loading ? 'Loading...': 'Sign Up'}</button>
       </form>
       <div className='flex gap-2 justify-center'>
         <h1>Have an account?</h1>
         <Link to={"/signin"} className='text-blue-700'>Sign in</Link>
       </div>
+      {error && <p className='text-red-900 flex justify-center'>{error}</p>}
     </div>
   )
 }
--- CLient/vite.config.js
+++ CLient/vite.config.js
@@ -4,6 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/Api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react(),
     tailwindcss(),
   ],
